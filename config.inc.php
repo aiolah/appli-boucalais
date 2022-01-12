8dd5348bc3141f9ec59bf59205bbb3f0af67a033
aiolah <58739714+aiolah@users.noreply.github.com>
2022-01-12 16:04:26 +0100

    Delete config.inc.php

--- config.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-// fichier de configuration pour l'accès à la base de données
-$server = 'leboucalais.fr.mysql';
-$user = 'leboucalais_fr';
-$passwd = 'xWLHki3L4GBzfCQ2M2GrvKHg';
-$database = 'leboucalais_fr';
-?>
